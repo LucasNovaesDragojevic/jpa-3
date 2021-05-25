2ede1cefe116de2f36aacd909be25cfd8a50f442
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-05-24 22:36:25 -0300

    Criados métodos de consulta

--- src/main/java/br/com/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/loja/dao/ProdutoDao.java
@@ -1,5 +1,8 @@
 package br.com.loja.dao;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.loja.modelo.Produto;
@@ -14,5 +17,35 @@ public class ProdutoDao {
 
     public void cadastrar(Produto produto) {
         this.em.persist(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "select p from Produto p";
+        return em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome) {
+        String jpql = "select p from Produto p where p.nome = :pNome";
+        return em.createQuery(jpql, Produto.class)
+                    .setParameter("pNome", nome)
+                    .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nome) {
+        String jpql = "select p from Produto p where p.categoria.nome = :pNome";
+        return em.createQuery(jpql, Produto.class)
+                    .setParameter("pNome", nome)
+                    .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+        String jpql = "select p.preco from Produto p where p.nome = :pNome";
+        return em.createQuery(jpql, BigDecimal.class)
+                    .setParameter("pNome", nome)
+                    .getSingleResult();
     }
 }
--- src/main/java/br/com/loja/testes/CadastroProduto.java
+++ src/main/java/br/com/loja/testes/CadastroProduto.java
@@ -13,6 +13,28 @@ import br.com.loja.util.JPAUtil;
 public class CadastroProduto {
 
     public static void main(String[] args) {
+        cadastrarProduto();
+
+        EntityManager em = JPAUtil.getEntityManager();
+        
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        
+        Produto produto = produtoDao.buscarPorId(1L);
+
+        System.out.println(produto);
+
+        produtoDao.buscarTodos().forEach(System.out::println);
+
+        produtoDao.buscarPorNome("Xiaomi Redmi").forEach(System.out::println);
+
+        produtoDao.buscarPorNomeDaCategoria("Celular").forEach(System.out::println);
+
+        BigDecimal preco = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        
+        System.out.println(preco);
+    }
+
+    private static void cadastrarProduto() {
         Categoria categoria = new Categoria("Celular");
 
         Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), categoria);
@@ -33,14 +55,14 @@ public class CadastroProduto {
         em.getTransaction().commit();
 
         // Transação 2
-        em.getTransaction().begin();
+        /*em.getTransaction().begin();
         
         celular.removerCategoria();
 
         categoriaDao.remover(categoria);
 
         em.getTransaction().commit();
-        
+        */
         em.close();
     }
 }
