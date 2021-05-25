529a296be8304c92cfc2848860bdc3fbb7d2705b
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-05-24 21:42:27 -0300

    Criado relacionamento entre entidades

--- src/main/java/br/com/loja/modelo/Produto.java
+++ src/main/java/br/com/loja/modelo/Produto.java
@@ -1,11 +1,15 @@
 package br.com.loja.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,36 +21,14 @@ public class Produto {
     private Long id;
     private String nome, descricao;
     private BigDecimal preco;
+    private LocalDate dataCadastro = LocalDate.now();
+    @ManyToOne
+    private Categoria categoria;
 
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
+    public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
         this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
         this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
         this.preco = preco;
+        this.categoria = categoria;
     }
 }
--- src/main/java/br/com/loja/testes/CadastroProduto.java
+++ src/main/java/br/com/loja/testes/CadastroProduto.java
@@ -3,25 +3,34 @@ package br.com.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
+import br.com.loja.JPAUtil;
+import br.com.loja.dao.CategoriaDao;
+import br.com.loja.dao.ProdutoDao;
+import br.com.loja.modelo.Categoria;
 import br.com.loja.modelo.Produto;
 
 public class CadastroProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Muito legal");
-        celular.setPreco(new BigDecimal("800"));
+        Categoria categoria = new Categoria("Celular");
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-        EntityManager em = factory.createEntityManager();
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), categoria);
+
+        EntityManager em = JPAUtil.getEntityManager();
         
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
         em.getTransaction().begin();
-        em.persist(celular);
+
+        categoriaDao.cadastrar(categoria);
+
+        produtoDao.cadastrar(celular);
+        
         em.getTransaction().commit();
+        
         em.close();
     }
 }
